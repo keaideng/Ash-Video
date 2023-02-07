7efe52ef73ca092341d1f76f8fc65c91cb4bd536
lainxin <3598556808@qq.com>
2023-02-07 14:28:05 +0800

    lu

--- unpackage/dist/dev/mp-weixin/pages/mine/mine.js
+++ unpackage/dist/dev/mp-weixin/pages/mine/mine.js
@@ -17,4 +17,5 @@ const _sfc_main = {
   }
 };
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "C:/\u4E0A\u8BFE\u6587\u4EF6/Class-documents/\u5C0F\u7A0B\u5E8F/\u5C0F\u667A\u89C6\u9891/pages/mine/mine.vue"]]);
+_sfc_main.__runtimeHooks = 2;
 wx.createPage(MiniProgramPage);
