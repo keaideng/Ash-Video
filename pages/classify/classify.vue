28bb2a7aa42d6ae63e6f3dc17f4e0585bab6fb26
lainxin <3598556808@qq.com>
2023-02-07 09:18:32 +0800

    陆

---
+++ pages/classify/classify.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script setup>
+// vue3小程序生命周期函数
+import { onShareAppMessage, onLoad, onShow, onHide } from '@dcloudio/uni-app';
+
+// 页面加载
+onLoad((message) => {
+	
+})
+
+// 页面显示
+onShow(() => {
+	
+})
+
+// 页面隐藏
+onHide(() => {
+	
+})
+
+// 页面分享(不定义该函数 页面将无法分享)
+onShareAppMessage(() => {
+	
+})
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ unpackage/dist/dev/mp-weixin/app.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperties(exports, { __esModule: { value: true }, [Symbol.toStringTag]: { value: "Module" } });
+const common_vendor = require("./common/vendor.js");
+if (!Math) {
+  "./pages/home/home.js";
+  "./pages/classify/classify.js";
+  "./pages/mine/mine.js";
+}
+const _sfc_main = {
+  onLaunch: function() {
+    console.log("App Launch");
+  },
+  onShow: function() {
+    console.log("App Show");
+  },
+  onHide: function() {
+    console.log("App Hide");
+  }
+};
+const App = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "C:/\u4E0A\u8BFE\u6587\u4EF6/Class-documents/\u5C0F\u7A0B\u5E8F/\u5C0F\u667A\u89C6\u9891/App.vue"]]);
+function createApp() {
+  const app = common_vendor.createSSRApp(App);
+  return {
+    app
+  };
+}
+createApp().app.mount("#app");
+exports.createApp = createApp;
