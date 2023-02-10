1659595fabe663370f8e3b86f21e3b1a32304314
xiaobangjie <1973744239@qq.com>
2023-02-10 11:01:26 +0800

    分类等

---
+++ branch/addvideo/addvideo.vue
@@ -0,0 +1,149 @@
+<template>
+	<view class="hmde">
+	  <view class="home">
+		<!-- 添加视频 -->
+		<view class="tjvideo">
+			<view class="imj">
+				<image src="../../static/img/c2c00f0cad1b7043b2054ff7d8b17fd.png"></image>
+			</view>
+			<text>点击添加视频</text>
+		</view>
+		<!-- 标题 | 描述 -->
+		<view class="label_describe">
+			<!-- 标题 -->
+			<view class="label">
+				<text>标题</text>
+				<input type="text" placeholder="请填写视频标题">
+			</view>
+			<!-- 描述 -->
+			<view class="describe">
+				<text>描述</text>
+				<input type="text" placeholder="请填写视频描述">
+			</view>
+		</view>
+		<!-- 完成 | 删除 -->
+		<view class="complete_delete">
+			<!-- 完成 -->
+			<view class="complete">
+				<text>完成</text>
+			</view>
+			<!-- 删除 -->
+			<view class="delete">
+				<text>删除</text>
+			</view>
+		</view>
+		<view class="bottom"></view>
+	</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	.hmde {
+		height: 1182rpx;
+		background-color: #F0F0F0;
+	}
+	.home {
+		width: 100%;
+		height: 864rpx;
+		.tjvideo {
+			height: 336rpx;
+			// background-color: #F0F0F0;
+			background-color: #F8F8F8;
+			text-align: center;
+			text {
+				line-height: 420rpx;
+				    font-size: 32rpx;
+				    color: grey;
+			}
+			.imj {
+				position: absolute;
+				    padding: 100rpx 345rpx;
+					image {
+						width: 58rpx;
+						height: 58rpx;
+					}
+			}
+		}
+		.label_describe {
+			height: 431rpx;
+			background-color: #FFF;
+			.label {
+				height: 90rpx;
+				border-bottom: 3rpx solid #E0E0E0;
+				display: flex;
+				text {
+					line-height: 90rpx;
+					font-size: 44rpx;
+					font-weight: bold;
+				    margin-left: 18rpx;
+				}
+				input {
+					font-size: 36rpx;
+					margin: 21rpx 30rpx;
+					border: 0rpx;
+				}
+			}
+			.describe {
+				height: 341rpx;
+				display: flex;
+					text {
+						line-height: 90rpx;
+						font-size: 44rpx;
+						font-weight: bold;
+					    margin-left: 18rpx;
+				}
+				input {
+					font-size: 36rpx;
+					margin: 21rpx 30rpx;
+					border: 0rpx;
+				}
+			}
+		}
+		.complete_delete {
+			height: 100rpx;
+			background-color: lightcoral;
+			display: flex;
+			border-top: 3rpx solid #E0E0E0;
+			.complete {
+				width: 375rpx;
+				height: 100rpx;
+				background-color: sandybrown;
+				text-align: center;
+				background-color: #FF6699;
+				text {
+					line-height: 100rpx;
+					font-size: 36rpx;
+					color: #FFF;
+				}
+			}
+			.delete {
+				width: 375rpx;
+				height: 100rpx;
+				background-color: lawngreen;
+				text-align: center;
+				background-color: #FFF;
+				text {
+					line-height: 100rpx;
+					font-size: 36rpx;
+				}
+			}
+		}
+	}
+	.bottom {
+		height: 17rpx;
+		background-color: #E8E8E8;
+	}
+</style>
--- pages/classify/classify.vue
+++ pages/classify/classify.vue
@@ -1,34 +1,247 @@
 <template>
-	<view>
-		
+	<view class="home">
+		<view class="search-nav">
+			<Search></Search>
+		</view>
+	<!-- 热门分类 -->
+	<view class="cation">
+			<view class="cation-item">
+				<text>热门分类</text>
+			</view>
+			<view class="classify">
+				<view class="l">
+					<view>
+						<image src='../../static/logo.png'></image>
+					</view>
+					<text>分类</text>
+				</view>
+				<view class="l">
+					<view>
+						<image src='../../static/logo.png'></image>
+					</view>
+					<text>分类</text>
+				</view>
+				<view class="l">
+					<view>
+						<image src='../../static/logo.png'></image>
+					</view>
+					<text>分类</text>
+				</view>
+				<view class="l">
+					<view>
+						<image src='../../static/logo.png'></image>
+					</view>
+					<text>分类</text>
+				</view>
+			</view>
+		</view>
+	</view>
+	<!-- 中间线 -->
+	<view class="qqq"></view>
+	<!-- 全部分类 -->
+	<view class="allsort">
+		<view class="sort">
+			<text>全部分类</text>
+		</view>
+		<view class="qb">
+			<view class="qb_item">
+				<view>
+					<image src='../../static/logo.png'></image>
+				</view>
+				<text>分类</text>
+			</view>
+			<view class="qb_item">
+				<view>
+					<image src='../../static/logo.png'></image>
+				</view>
+				<text>分类</text>
+			</view>
+			<view class="qb_item">
+				<view>
+					<image src='../../static/logo.png'></image>
+				</view>
+				<text>分类</text>
+			</view>
+			<view class="qb_item">
+				<view>
+					<image src='../../static/logo.png'></image>
+				</view>
+				<text>分类</text>
+			</view>
+		</view>
+		<view class="bq">
+			<view class="bq_item">
+				<view>
+					<image src='../../static/logo.png'></image>
+				</view>
+				<text>分类</text>
+			</view>
+			<view class="bq_item">
+				<view>
+					<image src='../../static/logo.png'></image>
+				</view>
+				<text>分类</text>
+			</view>
+			<view class="bq_item">
+				<view>
+					<image src='../../static/logo.png'></image>
+				</view>
+				<text>分类</text>
+			</view>
+			<view class="bq_item">
+				<view>
+					<image src='../../static/logo.png'></image>
+				</view>
+				<text>分类</text>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script setup>
-// vue3小程序生命周期函数
-import { onShareAppMessage, onLoad, onShow, onHide } from '@dcloudio/uni-app';
+	// vue3小程序生命周期函数
+	import {
+		onShareAppMessage,
+		onLoad,
+		onShow,
+		onHide
+	} from '@dcloudio/uni-app';
+	import Search from "../../components/Search/index.vue"
 
-// 页面加载
-onLoad((message) => {
-	
-})
+	// 页面加载
+	onLoad((message) => {
 
-// 页面显示
-onShow(() => {
-	
-})
+	})
 
-// 页面隐藏
-onHide(() => {
-	
-})
+	// 页面显示
+	onShow(() => {
 
-// 页面分享(不定义该函数 页面将无法分享)
-onShareAppMessage(() => {
-	
-})
+	})
+
+	// 页面隐藏
+	onHide(() => {
+
+	})
+
+	// 页面分享(不定义该函数 页面将无法分享)
+	onShareAppMessage(() => {
+
+	})
 </script>
 
 <style lang="scss">
+	.home {
+		padding: 0rpx 20rpx;
+	}
+	.search-nav {
+		display: flex;
+		margin-top: 10rpx;
+		color: #656771;
 
+		text {
+			margin: 20rpx 0rpx 0rpx 10rpx;
+		}
+	}
+	.cation {
+		margin-top: 40rpx;
+		height: 300rpx;
+
+		.cation-item {
+			height: 40rpx;
+			padding: 0rpx 14rpx;
+			text {
+				font-weight: bold;
+				font-size: 40rpx;
+			}
+		}
+	}
+	.classify {
+		width: 650rpx;
+		height: 180rpx;
+		display: flex;
+		justify-items: center;
+		margin-top: 36rpx;
+
+		.l {
+			width: 100rpx;
+			height: 100rpx;
+			line-height: 80rpx;
+			margin: 42rpx;
+			margin-right: 37rpx;
+			image {
+				width: 100rpx;
+				height: 100rpx;	
+				// margin-right: 54rpx;
+				margin-bottom: -30rpx;
+			}
+			text {
+				font-size: 38rpx;
+				color: #656771;
+				margin-left: 10rpx;
+			}
+		}
+	}
+	.qqq {
+		width: 800rpx;
+		height: 20rpx;
+		background-color: #F7F7F7;
+	}
+	.allsort {
+		width: 750rpx;
+		height: 615rpx;
+		.sort {
+			width: 100%;
+			height: 70rpx;
+			padding: 20rpx 20rpx;
+			text {
+				font-weight: bold;
+				font-size: 40rpx;
+				margin-left: 14rpx;
+			}
+		}
+		.qb {
+			width: 100%;
+			// height: 272rpx;
+			height: 225rpx;
+			margin-left: 54rpx;
+			margin-top: 10px;
+			display: flex;
+			.qb_item {
+				width: 160rpx;
+				height: 267rpx;
+				margin: 9rpx;
+				image {
+					width: 100rpx;
+					height: 100rpx;	
+					margin-right: 54rpx;
+				}
+				text {
+					font-size: 38rpx;
+					color: #656771;
+					margin-left: 10rpx;
+				}
+			}
+		}
+		.bq {
+			width: 100%;
+			height: 200rpx;
+			margin-left: 54rpx;
+			display: flex;
+			.bq_item {
+				width: 160rpx;
+				height: 267rpx;
+				margin: 9rpx;
+				image {
+					width: 100rpx;
+					height: 100rpx;	
+					margin-right: 54rpx;
+				}
+				text {
+					font-size: 38rpx;
+					color: #656771;
+					margin-left: 10rpx;
+				}
+			}
+		}
+	}
 </style>
